4bc55849b370983dcea7153d8cfbe0011ffdd30b
iSm1le <ism1le@xaff.ru>
2017-10-21 12:59:31 +0300

    Changed travis.yml
    Signed-off-by: Mikhail K <ism1le@xaff.ru>

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -2,10 +2,6 @@
 
 set -e
 
-function test {
-	npm test
-}
-
 if [[ "$TRAVIS_BRANCH" == revert-* ]]; then
 	echo -e "\e[36m\e[1mBuild triggered for reversion branch \"${TRAVIS_BRANCH}\" - running nothing."
 	exit 0
@@ -13,14 +9,11 @@ fi
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
 	echo -e "\e[36m\e[1mBuild triggered for PR #${TRAVIS_PULL_REQUEST} to branch \"${TRAVIS_BRANCH}\" - only running test."
-	test
 	exit 0
 fi
 
 if [ -n "$TRAVIS_TAG" ]; then
 	echo -e "\e[36m\e[1mBuild triggered for tag \"${TRAVIS_TAG}\"."
-	test
 else
 	echo -e "\e[36m\e[1mBuild triggered for branch \"${TRAVIS_BRANCH}\"."
-	test
 fi
